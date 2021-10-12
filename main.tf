35a55df23ee2e084e2196e3ed32b584d47c0a31a
Rostislav Skripnik <rostislav.skripnik@dataart.com>
2021-10-12 12:17:02 +0300

    Update pipline file

--- main.tf
+++ main.tf
@@ -1,6 +1,25 @@
+
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 2.65"
+    }
+  }
+    required_version = ">= 0.14.9"
+}
+
 provider "azurerm" {
-    version = "2.80.0"
     features {}
+}
+
+terraform {
+    backend "azurerm" {
+        resource_group_name  = "tf_rg_blobstore"
+        storage_account_name = "tfstoragerost"
+        container_name       = "tfstate"
+        key                  = "terraform.tfstate"
+    }
 }
 
 resource "azurerm_resource_group" "tf_test" {
